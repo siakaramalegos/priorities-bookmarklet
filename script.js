0e41f1dac23a074ba9cc16995f902a6265b13fcd
Sia <siakaramalegos@gmail.com>
2024-01-03 16:35:09 -0500

    Separate priority levels and add styling

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 const container=document.createElement("div")
-const fetchpriorityList = document.querySelectorAll("[fetchpriority]")
+container.style["padding"] = "20px"
+container.style["z-index"] = "9999"
+container.style["background-color"] = "#FFF"
+container.style["color"] = "#222"
+container.style["box-shadow"] = "0 3px 6px rgba(0,0,0,.15), 0 2px 4px rgba(0,0,0,.12)"
+container.style["position"] = "absolute"
+container.style["inset"] = "20px"
+container.style["overflow"] = "scroll"
+container.style["font-size"] = "16px"
 
-const heading=document.createElement("p")
-heading.innerHTML = "Elements with fetchpriority set:"
-const ol = document.createElement("ol")
+const h1 = document.createElement("h1")
+h1.innerHTML = "Priorities Scanner Results"
+h1.style["font-size"] = "24px"
+const info = document.createElement("p")
+info.innerHTML = 'Refresh the page to remove this overlay. <a href="https://projects.sia.codes/priorities-bookmarklet/">Share</a> this bookmarklet.'
+container.append(h1)
+container.append(info)
 
-fetchpriorityList.forEach(e => {
+const FETCHPRIORITY_CHECKS = [
+  {
+    selector: '[fetchpriority="high"]',
+    headingText: "Elements with fetchpriority set to high:"
+  },
+  {
+    selector: '[fetchpriority="low"]',
+    headingText: "Elements with fetchpriority set to low:"
+  },
+]
+
+FETCHPRIORITY_CHECKS.forEach(check => {
+  const nodelist = document.querySelectorAll(check.selector)
+
+  const heading=document.createElement("h2")
+  heading.innerHTML = check.headingText
+  heading.style["font-size"] = "20px"
+  const ol = document.createElement("ol")
+
+  nodelist.forEach(e => {
     const li = document.createElement("li")
     const value = e.href ? `with href="${e.href}"` : `with src="${e.src}"`
     li.innerHTML = `${e.nodeName} ${value}`
     ol.append(li)
-})
+  })
 
-container.append(heading)
-container.append(ol)
+  container.append(heading)
+  container.append(ol)
+})
 
 const CHECKS = [
   {
@@ -37,8 +69,9 @@ const CHECKS = [
 CHECKS.forEach(check => {
   const nodelist = document.querySelectorAll(check.selector)
 
-  const heading=document.createElement("p")
+  const heading=document.createElement("h2")
   heading.innerHTML = check.headingText
+  heading.style["font-size"] = "20px"
   const ol = document.createElement("ol")
 
   nodelist.forEach(e => {
